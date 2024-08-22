ff1bdcfa03d2a5b6da3bea547a5a0ec091577e11
bigreni <santhanakrishnan.sai@gmail.com>
2024-08-21 19:19:53 -0500

    Update Route List

--- www/js/mainpro.js
+++ www/js/mainpro.js
@@ -10,9 +10,9 @@ function initApp() {
     if (/(android)/i.test(navigator.userAgent)){
         interstitial = new admob.InterstitialAd({
             //dev
-            adUnitId: 'ca-app-pub-3940256099942544/1033173712'
+            //adUnitId: 'ca-app-pub-3940256099942544/1033173712'
             //prod
-            //adUnitId: 'ca-app-pub-9249695405712287/9175491250'
+            adUnitId: 'ca-app-pub-9249695405712287/9175491250'
           });
         }
         else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent) || (navigator.userAgent.includes("Mac") && "ontouchend" in document)) {
